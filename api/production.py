67e65273c3ab018646215aac8f145871916787ee
matr@grapeup.com <matr@grapeup.com>
2020-09-19 21:08:39 +0200

    updates to api

--- api/production.py
+++ api/production.py
@@ -1,10 +1,17 @@
-from flask_restplus import Namespace, Resource
+from flask_restplus import Namespace, Resource, marshal
 
-from db import ProductionDao
-from .model import ProductionModel as ProductionModel
+from service import ProductionSvc
+from view.decorator import modal_result
+from .model import ProductionModel
 
 api = Namespace('production')
 api.add_model('Production', ProductionModel)
+
+parser = api.parser()
+parser.add_argument('type')
+parser.add_argument('worker', action='append')
+parser.add_argument('tool', action='append')
+parser.add_argument('resource', action='append')
 
 
 @api.route('')
@@ -12,4 +19,16 @@ class ProductionList(Resource):
     @api.doc('list all')
     @api.marshal_list_with(ProductionModel)
     def get(self):
-        return ProductionDao.read_all()
+        return ProductionSvc.read_all()
+
+    @api.doc('create')
+    @modal_result
+    def post(self):
+        args = parser.parse_args()
+        type = args.get('type')
+        workers = args.get('worker') if args.get('worker') is not None else []
+        tools = args.get('tool') if args.get('tool') is not None else []
+        resources = args.get('resource') if args.get('resource') is not None else []
+
+        result = ProductionSvc.start_production(type, workers=workers, tools=tools, resources=resources)
+        return marshal(result, ProductionModel, skip_none=True)
